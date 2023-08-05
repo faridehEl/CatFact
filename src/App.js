32365fdadbb4cbdc0cff1d117211add275a55d24
feri <rht.farideh@yahoo.com>
2023-08-05 17:16:09 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import './App.css';
 import Axios from 'axios';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
 const [catFact , setCatFact] = useState("")
+const [showCatFact,setshowCatFact]=useState('true')
 
-useEffect(()=>{
-  showFact()
-},[]);
+// useEffect(()=>{
+//   showFact()
+// },[]);
 const showFact=()=>{
     Axios.get("https://catfact.ninja/fact").then(res=>{
     setCatFact(res.data.fact)
   });
+  setshowCatFact('false')
 }
 
   return (
    <div className='showData'> 
-    <p>{catFact}</p>
+       <Router>
+      <Routes>
+        <Route  path='/home' element={<Home></Home>}/>
+        <Route  path='/about' element={<About></About>}/>
+      </Routes>
+    </Router>
     <button onClick={showFact}>Fetch Data</button>
+    {showCatFact === 'false' ?  <p>{catFact}</p> : <p>NO message</p>}
    </div>
     
   );
---
+++ src/Pages/About.js
@@ -0,0 +1,10 @@
+const About=()=>{
+    return(
+        <div>
+            <h3>
+                Hi It is test of Aboutpage 
+            </h3>
+        </div>
+    )
+}
+export default About
